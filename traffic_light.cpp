f3bba77e73531a3d01d31dc040e776c7733d117f
kp-_-22 <klphillips9@crimson.ua.edu>
2025-12-18 17:43:27 -0600

    Main needs work,traffic_light.cpp/.h fuctions done

--- traffic_light.cpp
+++ traffic_light.cpp
@@ -1,11 +1,40 @@
-#include <iostream>
+//traffic_light.cpp
+#include "traffic_light.h"
 
+TrafficLight::TrafficLight(int green, int yellow, int red) : 
+state("RED"), timer(0), Greentime(green), Yellowtime(yellow), Redtime(red) {}
 
-using namespace std;
+void TrafficLight::update(){
+    timer++;
 
-int main(){
-    
+    if(state == "GREEN" && timer >= Greentime){
+        state = "YELLOW";
+        timer = 0;
+    }
+    else if(state == "YELLOW" && timer >= Yellowtime){
+        state = "RED";
+        timer = 0;
+    }
+    else if(state == "RED" && timer >= Redtime){
+        state = "GREEN";
+        timer = 0;
+    }
+}
 
+string TrafficLight::getState() const{
+    return state;
+}
 
-    return 0;
+void TrafficLight::reset(){
+    state = "RED";
+    timer = 0;
+}
+
+void TrafficLight::setState(const string& newState){
+    state = newState;
+    timer = 0;
+}
+
+int TrafficLight::SecsRed(){
+    SecondsRed++;
 }
